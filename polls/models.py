6e1a3572a9b0f4c8a0ac2c27fd6bb31e7af9bfef
Phil Shao <shaoster@gmail.com>
2025-12-16 21:02:04 -0500

    feat: Tutorial02
    Finishes the setup in tutorial02 and adds some additional
    helper commands to the setup.sh environment.

--- polls/models.py
+++ polls/models.py
@@ -4,4 +4,21 @@ from django.db import models
 from django.utils import timezone
 
 
-# Create your models here.
+class Question(models.Model):
+    question_text: models.CharField = models.CharField(max_length=200)
+    pub_date: models.DateTimeField = models.DateTimeField("date published")
+
+    def __str__(self) -> str:
+        return self.question_text
+
+    def was_published_recently(self) -> bool:
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question: models.ForeignKey = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text: models.CharField = models.CharField(max_length=200)
+    votes: models.IntegerField = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
