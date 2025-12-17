3e8d54905582c5d4d24382cf987c0872854324eb
Phil Shao <shaoster@gmail.com>
2025-12-16 23:14:04 -0500

    Tutorial05: Add tests
    - Add unit tests to the pre-push check.
    - Expand README.md accordingly.

--- polls/tests.py
+++ polls/tests.py
@@ -26,7 +26,6 @@ class QuestionModelTests(TestCase):
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
-
     def test_was_published_recently_with_recent_question(self):
         """
         was_published_recently() returns True for questions whose pub_date
@@ -36,6 +35,7 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
@@ -44,6 +44,7 @@ def create_question(question_text, days):
     """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
